e8703346a44c0bd74feacab0be4668dcd9ca47cf
이태호 <leetag1163@naver.com>
2024-03-30 20:02:00 +0900

    feat: Security 적용, Auth 구글 회원가입 및 로그인 구현 (squash merge from branch 'be/feature/auth/S10P22A404-156/auth')

--- Backend/funco/src/main/java/com/found_404/funco/member/domain/repository/MemberRepository.java
+++ Backend/funco/src/main/java/com/found_404/funco/member/domain/repository/MemberRepository.java
@@ -1,8 +1,14 @@
 package com.found_404.funco.member.domain.repository;
 
-import com.found_404.funco.member.domain.Member;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+import com.found_404.funco.auth.dto.OauthId;
+import com.found_404.funco.member.domain.Member;
 
+public interface MemberRepository extends JpaRepository<Member, Long> {
+	Optional<Member> findByOauthId(OauthId oauthId);
+
+	boolean existsByNickname(String nickname);
 }
